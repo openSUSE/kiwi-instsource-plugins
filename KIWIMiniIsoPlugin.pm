594916f39c7817421524ed6954ccf94dbe2c5946
Adrian Schröter <adrian@suse.de>
2016-10-18 11:54:22 +0200

    13.2 import

--- KIWIMiniIsoPlugin.pm
+++ KIWIMiniIsoPlugin.pm
@@ -142,8 +142,7 @@ sub execute {
         $this->removeMediaCheck($isolxfiles[0]);
     }
 
-    $this -> updateInitRDNET("./etc/linuxrc.d/10_repo", "defaultrepo=$repoloc\n");
-    $this -> updateInitRDNET("./etc/linuxrc.d/15_kexec", "kexec=1\n");
+    $this -> updateInitRDNET($repoloc);
 
     my @gfxbootfiles;
     find(
@@ -321,7 +320,7 @@ sub _makecpiohead {
 # download.opensuse.org
 # https://bugzilla.opensuse.org/show_bug.cgi?id=916175
 sub updateInitRDNET {
-    my ($this, $file, $content) = @_;
+    my ($this, $repoloc) = @_;
 
     $this -> logMsg("I", "prepare initrd for NET iso");
 
@@ -332,8 +331,10 @@ sub updateInitRDNET {
     # hardcode for now
     $zipper = "xz --check=crc32";
 
-    my ($cpio, $pad) = _makecpiohead($file, [0, 0, oct(644), 1, 0, 0, 0, length($content), 0, 0, 0]);
-    $cpio .= $content;
+    my $linuxrc = "defaultrepo=$repoloc\n";
+
+    my ($cpio, $pad) = _makecpiohead('./etc/linuxrc.d/10_repo', [0, 0, oct(644), 1, 0, 0, 0, length($linuxrc), 0, 0, 0]);
+    $cpio .= $linuxrc;
     $cpio .= $pad if $pad;
     $cpio .= _makecpiohead();
 
